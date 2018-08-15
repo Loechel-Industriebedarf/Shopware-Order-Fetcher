947cee9502904e3676ac241a8ff95c8d2c7c4998
ADarkHero <maik.riedlsperger@gmail.com>
2018-08-15 13:19:14 +0200

    Shipping isn't imported as "net" now.

--- index.php
+++ index.php
@@ -1,126 +1,5 @@
 <?php
-class ApiClient
-{
-    const METHOD_GET = 'GET';
-    const METHOD_PUT = 'PUT';
-    const METHOD_POST = 'POST';
-    const METHOD_DELETE = 'DELETE';
-    protected $validMethods = [
-        self::METHOD_GET,
-        self::METHOD_PUT,
-        self::METHOD_POST,
-        self::METHOD_DELETE,
-    ];
-    protected $apiUrl;
-    protected $cURL;
-
-    public function __construct($apiUrl, $username, $apiKey)
-    {
-        $this->apiUrl = rtrim($apiUrl, '/') . '/';
-        //Initializes the cURL instance
-        $this->cURL = curl_init();
-        curl_setopt($this->cURL, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->cURL, CURLOPT_FOLLOWLOCATION, false);
-        curl_setopt($this->cURL, CURLOPT_USERAGENT, 'Shopware ApiClient');
-        curl_setopt($this->cURL, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
-        curl_setopt($this->cURL, CURLOPT_USERPWD, $username . ':' . $apiKey);
-        curl_setopt(
-            $this->cURL,
-            CURLOPT_HTTPHEADER,
-            ['Content-Type: application/json; charset=utf-8']
-        );
-    }
-
-    public function call($url, $method = self::METHOD_GET, $data = [], $params = [])
-    {
-        if (!in_array($method, $this->validMethods)) {
-            throw new Exception('Invalid HTTP-Methode: ' . $method);
-        }
-        $queryString = '';
-        if (!empty($params)) {
-            $queryString = http_build_query($params);
-        }
-        $url = rtrim($url, '?') . '?';
-        $url = $this->apiUrl . $url . $queryString;
-        $dataString = json_encode($data);
-        curl_setopt($this->cURL, CURLOPT_URL, $url);
-        curl_setopt($this->cURL, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($this->cURL, CURLOPT_POSTFIELDS, $dataString);
-        $result = curl_exec($this->cURL);
-        $httpCode = curl_getinfo($this->cURL, CURLINFO_HTTP_CODE);
-
-        return $this->prepareResponse($result, $httpCode);
-    }
-
-    public function get($url, $params = [])
-    {
-        return $this->call($url, self::METHOD_GET, [], $params);
-    }
-
-    public function post($url, $data = [], $params = [])
-    {
-        return $this->call($url, self::METHOD_POST, $data, $params);
-    }
-
-    public function put($url, $data = [], $params = [])
-    {
-        return $this->call($url, self::METHOD_PUT, $data, $params);
-    }
-
-    public function delete($url, $params = [])
-    {
-        return $this->call($url, self::METHOD_DELETE, [], $params);
-    }
-
-    protected function prepareResponse($result, $httpCode)
-    {
-        echo "HTTP: $httpCode";
-        if (null === $decodedResult = json_decode($result, true)) {
-            $jsonErrors = [
-                JSON_ERROR_NONE => 'No error occurred',
-                JSON_ERROR_DEPTH => 'The maximum stack depth has been reached',
-                JSON_ERROR_CTRL_CHAR => 'Control character issue, maybe wrong encoded',
-                JSON_ERROR_SYNTAX => 'Syntaxerror',
-            ];
-            echo 'Could not decode json';
-            echo 'json_last_error: ' . $jsonErrors[json_last_error()];
-            echo '<br>Raw:<br>';
-            echo '<pre>' . print_r($result, true) . '</pre>';
-
-            return;
-        }
-        if (!isset($decodedResult['success'])) {
-            echo 'Invalid Response';
-
-            return;
-        }
-        if (!$decodedResult['success']) {
-            echo 'No Success';
-            echo '<p>' . $decodedResult['message'] . '</p>';
-            if (array_key_exists('errors', $decodedResult) && is_array($decodedResult['errors'])) {
-                echo '<p>' . join('</p><p>', $decodedResult['errors']) . '</p>';
-            }
-
-            return;
-        }
-        echo 'Success';
-        if (isset($decodedResult['data'])) {
-			//Show order array
-            echo '<pre>' . print_r($decodedResult['data'], true) . '</pre>';
-        }
-
-        return $decodedResult;
-    }
-}
-
-$client = new ApiClient(
-    //URL of shopware REST server
-    '',
-    //Username
-    '',
-    //User's API-Key
-    ''
-);
+require_once 'inc/shopware-api.php';
 
 $logfile = 'shopware.log';
 $csvfile = '../shopwareOrder.csv';
@@ -184,7 +63,7 @@ if(!file_exists($csvfile)){
 			array_push($list, array(
 			$order_data["number"], $order_data['invoiceAmountNet'], 
 			$item["articleNumber"], $item["price"], $item["quantity"],
-			$order_payment["description"], $order_data["invoiceShippingNet"],
+			$order_payment["description"], $order_data["invoiceShipping"],
 			$billing_adress, $order_billing["street"], $order_billing["zipCode"], $order_billing["city"],
 			$order_billing["country"]["name"], $order_billing["country"]["iso"],
 			$shipping_adress, $order_shipping["street"], $order_shipping["zipCode"], $order_shipping["city"],
